37d772c5d8a7f88083dd4530cc6252ea3d3fb984
ntflores <100636782+ntflores@users.noreply.github.com>
2022-05-02 13:44:54 -0700

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,12 @@
+def gradeText(grade):
+    if grade >= 90:
+        print("You got an A")
+    elif grade >= 80:
+        print("You got a B")
+    elif grade >= 70:
+        print("You got a C")
+    elif grade >= 60:
+        print("You got a D")
+    else:
+        print("You failed")
+
